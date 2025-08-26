0f9a6a167689f508f544336cf6202d5958d5fdfd
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-08-26 11:06:16 +0530

    arrays started

---
+++ Practise/MyArrays/Main.java
@@ -0,0 +1,16 @@
+package MyArrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Primitive 
+        //Arrays are in static in nature - length cannot inc dynamically.
+        int [] arr = new int[2] ;
+        arr[0]=1;
+        arr[1]=2;
+        System.out.println(arr );
+
+    }
+    
+}
